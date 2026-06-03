1640719b1f6be2769d4a6ce227f7ad60221a452a
ahmedWessamTest <ahmedwessam202@gmail.com>
2026-06-03 17:03:29 +0200

    feat: add netlify redirects, implement navbar component, add base styles, and include new assets

--- src/app/shared/components/advertising-area/advertising-area.component.ts
+++ src/app/shared/components/advertising-area/advertising-area.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-advertising-area',
   imports: [CommonModule, RouterLink],
   templateUrl: './advertising-area.component.html',
-  styleUrl: './advertising-area.component.scss'
+  styleUrl: './advertising-area.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdvertisingAreaComponent {
-  @Input() positionClasses: string = '';
-  @Input() imageWidth: string = '220px';
+  positionClasses = input<string>('');
+  imageWidth = input<string>('220px');
 }
